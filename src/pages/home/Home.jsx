8224dc3af148772bee9d627032c60e75696f582e
anobes6416 <sebonaifa777@gmail.com>
2023-10-26 13:37:25 -0400

    kankot react

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Featured from "../../components/featured/Featured";
+import "./Home.scss";
+import TrustedBy from '../../components/trustedBy/TrustedBy';
+const Home = () => {
+  return (
+    <div className='home'>
+      <Featured />
+      <TrustedBy />
+    </div>
+  )
+}
+
+export default Home
